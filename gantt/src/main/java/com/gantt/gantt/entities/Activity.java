a60a2dcad69473df531a487897001bd201d84f47
gmdeorozco <ernesto.orozco.coulson@gmail.com>
2022-12-18 06:25:27 +0000

    firs commit

---
+++ gantt/src/main/java/com/gantt/gantt/entities/Activity.java
@@ -0,0 +1,7 @@
+package com.gantt.gantt.entities;
+
+public class Activity {
+    private Long id;
+    private List<Activity> prerequisites;
+
+}
---
+++ gantt/src/test/java/com/gantt/gantt/GanttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gantt.gantt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GanttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
